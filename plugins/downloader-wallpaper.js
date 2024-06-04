ceb5bb29413498db9ed444c177981c503c01a67b
ulqui0orra <fahdulquiorra@gmail.com>
2024-06-05 00:30:34 +0300

    Update downloader-wallpaper.js

--- plugins/downloader-wallpaper.js
+++ plugins/downloader-wallpaper.js
@@ -1,17 +1,21 @@
-import {wallpaper} from '@bochilteam/scraper';
+import {pinterest} from '@bochilteam/scraper';
+
 
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.downloader_wallpaper
+  const tradutor = _translate.plugins.downloader_pinterest
+
 
   if (!text) throw `${tradutor.texto1} ${usedPrefix + command} Minecraft*`;
-  const res = await wallpaper(text);
-  const img = res[Math.floor(Math.random() * res.length)];
-  conn.sendFile(m.chat, img, 'error.jpg', `${tradutor.texto2} ${text}*`, m);
+  const json = await pinterest(text);
+  conn.sendFile(m.chat, json.getRandom(), 'error.jpg', `
+${tradutor.texto2}
+${text}
+`.trim(), m);
 };
-handler.help = ['', '2'].map((v) => 'wallpaper' + v + ' <query>');
-handler.tags = ['downloader'];
-handler.command = /^(wallpaper2?)$/i;
+handler.help = ['pinterest <keyword>'];
+handler.tags = ['internet'];
+handler.command = /^(بينترست)$/i;
 export default handler;
