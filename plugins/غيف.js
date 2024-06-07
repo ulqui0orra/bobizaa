7b6ebfe5e4782db33528b8ec5c7bcaba3b5ff35b
ulqui0orra <fahdulquiorra@gmail.com>
2024-06-07 21:15:46 +0300

    Create غيف.js

---
+++ plugins/غيف.js
@@ -0,0 +1,13 @@
+const handler = async (m, {conn, usedPrefix, command}) => {
+
+  if (!m.quoted) throw '❗الرجاء الرد على فيديو';
+  const q = m.quoted || m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!/(mp4)/.test(mime)) throw '⚠️ خطأ';
+  m.reply(global.wait);
+  const media = await q.download();
+  conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: 'تم طلبك'}, {quoted: m});
+};
+
+handler.command = ['togif', 'غيف', 'لغيف' , 'gif'];
+export default handler;
