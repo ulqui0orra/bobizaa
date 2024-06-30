2a60584767baf8ddb78b5f58912bde2966b5d133
ulqui0orra <fahdulquiorra@gmail.com>
2024-06-30 21:03:01 +0300

    Update حضن.js

--- plugins/حضن.js
+++ plugins/حضن.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, usedPrefix, command, text}) => {
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : false;
   else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat;
   const user = global.db.data.users[who];
-  if (!who) return m.reply(`✳️ منشن اللي عايز تحضنه\n${usedPrefix + command}`);
+  if (!who) return m.reply(`✳️ منشن اللي عايز تحضنه`);
 
   const abrazo = await conn.reply(m.chat, `@${m.sender.split('@')[0]}  يحضن @${who.split('@')[0]} `, m, {mentions: [who, m.sender]});
 
