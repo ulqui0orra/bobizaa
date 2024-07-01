5df9a9facc1fe14934c350c6de3c38945d56f5bd
ulqui0orra <fahdulquiorra@gmail.com>
2024-07-01 19:43:50 +0300

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,20 +1,15 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const fs = require('fs');
+let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+    m.reply(${text ? `${text}\n : ''}✦━━━━━━[ 𝕌𝕝𝕢𝕦𝕚𝕠𝕣𝕣𝕒 ]━━━━━━✦
+⤹🔮⤸
+❂━━━ • ━━━╏🔮╏━━━ • ━━━❂
 
-const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command, usedPrefix }) => {
-  const datas = global;
-  const idioma = datas.db.data.users[m.sender].language;
-  const _translate = JSON.parse(fs.readFileSync(./language/${idioma}.json));
-  const tradutor = _translate.plugins.gc_tagall;
-
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
-  if (!(isAdmin || isOwner)) {
-    global.dfail('admin', m, conn);
-    throw false;
-  }
-
-  const userId = m.sender;
+⤹🔮 المنشن ⊰⚡⊱ الجماعي 🔮⤸
+\n` + users.map(v => '│◦❈↲ منور :) @' + v.replace(/@.+/, '')).join`\n` + '\n*✦━━━━━━[ 𝕌𝕝𝕢𝕦𝕚𝕠𝕣𝕣𝕒 ]━━━━━━✦*', null, {
+        mentions: users
+    })
+}
+ const userId = m.sender;
   const currentTime = Date.now();
   const cooldownTime = 5 * 60 * 60 * 1000; // 5 ساعات
 
@@ -30,28 +25,10 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
     m.reply(يرجى الانتظار ${hours} ساعة و ${minutes} دقيقة و ${seconds} ثانية قبل استخدام هذا الأمر مرة أخرى.);
     return;
   }
+handler.help = ['منشن']
+handler.tags = ['group']
+handler.command = ['منشن']
+handler.admin = true
+handler.group = true
 
-  global.tagallCooldowns[userId] = currentTime;
-
-  const pesan = args.join` `;
-  const oi = ${tradutor.texto1[0]} ${pesan};
-  let teks = ${tradutor.texto1[1]}  ${oi}\n\n${tradutor.texto1[2]}\n;
-
-  for (const mem of participants) {
-    const user = global.db.data.users[mem.id] || { registered: false };
-    const name = user.name || 'مستخدم غير مسجل';
-    const regStatus = user.registered ? '' : '❌';
-    teks += ┣➥ @${mem.id.split('@')[0]} \n;
-  }
-
-  teks += *└*  𝕌𝕝𝕢𝕦𝕚𝕠𝕣𝕣𝕒 \n\n*❂━━━ • ━━━ • ━━━ • ━━━❂*;
-  conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) });
-};
-
-handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
-handler.tags = ['group'];
-handler.command = /^منشن$/i;
-handler.admin = true;
-handler.group = true;
-
-export default handler;
+export default handler
