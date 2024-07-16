45caa53d163d0108d71b331cc1f39711972a5790
ulqui0orra <fahdulquiorra@gmail.com>
2024-07-16 21:05:07 +0300

    Update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -1,49 +1,58 @@
 let handler = m => m; 
-
+  
  handler.all = async function (m) { 
-  let chat = global.db.data.chats[m.chat]; 
-  let responses; 
-  if (/^الكيورا$/i.test(m.text)) { 
-    responses = [ 
-                  '؟',
-                      'تفضل.',
-                             'نعم؟',
- ]; 
- } else if (/^بوووت|بموووووووووت|بوت$/i.test(m.text)) { 
-     responses = [ 
-'تفضل.',
-   '؟',
-   'نعم؟',
-      '*البوت شغال.*',
-     ]; 
-   } else if (/^اوامر$/i.test(m.text)) { 
-     responses = [ 
-       '*لا تنسى ال .*',
-     ];
- }else if (/^كيورا$/i.test(m.text)) { 
-     responses = [ 
-       'اللقب اسمه الكيورا و ليس كيورا.',
-     ];
-   }else if (/^هلا|هاي|هلو|هلوز|اهلا$/i.test(m.text)) { 
-     responses = [ 
-       'قول السلام عليكم افضل.',
-     ];
-   }else if (/^كيفك|كيف حالك$/i.test(m.text)) { 
-     responses = [ 
-       'الحمدلله.',  
-             'الحمدلله علي كل شيء.',  
-                   'الحمدلله بخير.'  
-                     'الحمدلله علي كل حال.',  
-                         'الحمدلله و انت؟.',  
-                           'الحمدلله علي كل شيء وانت؟',  
-                             'الحمدلله علي كل حال و انت؟',
-     ];
- }
- if (responses) { 
- let randomIndex = Math.floor(Math.random() * responses.length); 
- conn.reply(m.chat, responses[randomIndex], m); 
- } 
- return !0 
+   let chat = global.db.data.chats[m.chat]; 
+   let responses; 
+   if (/^بحبك|بعشقك|بموت فيك$/i.test(m.text)) { 
+     responses = [ 
+       'شكرا',  
+       'اموت فيك يا قلبي',  
+             'اعشقككككككك',  
+                   'احبكككك',  
+                       'لا دز '  
+     ]; 
+} else if (/^بوووووووووت|بوت|بوووت|$/i.test(m.text)) { 
+     responses = [ 
+       '*تفضل.*',  
+              '*؟*',
+                  '*نعم؟*', 
+                    '*البوت شغال.*',
+                      '*البوت اسمه الكيورا.*'
+]; 
+  }else if (/^الكيورا$/i.test(m.text)) { 
+     responses = [ 
+       '*؟*',  
+          '*تفضل.*',  
+               '*نعم؟*',  
+    ]; 
+  }else if (/^اوامر/i.test(m.text)) { 
+     responses = [ 
+              '*لا تنسي ال.*'   
+     ]; 
+   }else if (/^كيفك|كيف حالك| شو اخبارك|كيفك؟|كيف حالك؟|شو اخبارك؟$/i.test(m.text)) { 
+     responses = [ 
+              '*الحمدلله.*', 
+                         '*الحمدلله علي كل شيء.*',  
+                          '*الحمدلله بخير.*',  
+                            '*الحمدلله علي كل حال.*',  
+                                '*الحمدلله و انت؟*',  
+                                '*الحمدلله علي كل شيء و انت؟*',  
+                                 '*الحمدلله علي كل حال و انت؟*'
+     ]; 
+ }else if (/^كيورا$/i.test(m.text)) { 
+     responses = [ 
+              '*اللقب اسمه الكيورا و ليس كيورا.*'   
+       ]; 
+   }else if (/^هاي|هلا|هلو|هلوز|اهلا$/i.test(m.text)) { 
+     responses = [ 
+              '*قول السلام عليكم افضل.*'   
+     ]; 
+    }  
+   if (responses) { 
+     let randomIndex = Math.floor(Math.random() * responses.length); 
+     conn.reply(m.chat, responses[randomIndex], m); 
+   } 
+   return !0 
  }; 
-
+  
  export default handler;
